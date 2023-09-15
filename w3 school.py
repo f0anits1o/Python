6076329412c7aa6f6c93d61bab36366342249c15
f0anits1o <avotrinainafidelis@gmail.com>
2023-09-15 15:59:47 +0300

    python and mysql: where

--- w3 school.py
+++ w3 school.py
@@ -770,3 +770,51 @@ mycursor.execute("SELECT * FROM customers")
 myresult = mycursor.fetchone()
 
 print(myresult)
+
+#4-4 Python MySQL Where
+
+#a
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="#k41W146)",
+  database="e_commerce"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers WHERE address = 'Park Lane 38'"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+  
+#b
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers WHERE address Like '%way%'"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+#c
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+
+mycursor.execute(sql, adr)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
