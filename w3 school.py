25f77b3e0ce00d3d52856589e721c9c0f943ded2
f0anits1o <avotrinainafidelis@gmail.com>
2023-09-15 16:29:15 +0300

    python and mysql: update and drop

--- w3 school.py
+++ w3 school.py
@@ -872,3 +872,27 @@ mycursor.execute(sql, adr)
 mydb.commit()
 
 print(mycursor.rowcount, "record(s) deleted")
+
+
+# 4-7 update
+#a
+mycursor = mydb.cursor()
+
+sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
+
+mycursor.execute(sql)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) affected")
+
+#b
+sql = "UPDATE customers SET address = %s WHERE address = %s"
+val = ("Valley 345", "Canyon 123")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) affected")
+
