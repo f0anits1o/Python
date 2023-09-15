de41650eb8329c300ae894021fd635178e775f0a
f0anits1o <avotrinainafidelis@gmail.com>
2023-09-15 16:16:56 +0300

    python and mysql: delete-o

--- w3 school.py
+++ w3 school.py
@@ -846,3 +846,29 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+  
+
+#4-6 Python MySQL Delete From By
+#a
+mycursor = mydb.cursor()
+
+sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
+
+mycursor.execute(sql)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
+
+#b
+
+mycursor = mydb.cursor()
+
+sql = "DELETE FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+
+mycursor.execute(sql, adr)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
